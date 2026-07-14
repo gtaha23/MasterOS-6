c2e8e53252cc852a0c5975b961e3fb35f625addd
gtaha230812 <gokhantahagpinar@gmail.com>
2026-07-14 13:42:29 +0300

    vga function typo fix

--- masterOS/src/drivers/vga/VGAF.h
+++ masterOS/src/drivers/vga/VGAF.h
@@ -10,6 +10,6 @@ void VGADFA(void); // Disable font access (for loading fonts)
 
 void VGALF(const uint8_t* font, uint32_t height); // Load font into VGA memory (font must be 256*height bytes)
 
-void VGAL8X8F(void); // Load 8x8 font into VGA memory
-void VGAL8X16F(void); // Load 8x16 font into VGA memory
+void VGALF8X8F(void); // Load 8x8 font into VGA memory
+void VGALF8X16F(void); // Load 8x16 font into VGA memory
 void VGACLS(uint8_t color);
